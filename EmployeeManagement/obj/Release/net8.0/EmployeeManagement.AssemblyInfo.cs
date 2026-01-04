a98237df08eb23f3721d0a6829ed7e1ff7431e78
Zuki <binhminer123@gmail.com>
2026-01-04 16:19:02 +0700

    Fixed bug

--- EmployeeManagement/obj/Release/net8.0/EmployeeManagement.AssemblyInfo.cs
+++ EmployeeManagement/obj/Release/net8.0/EmployeeManagement.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagement")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39e95f7d70109055beea7ae570f564462dec859d")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagement")]
